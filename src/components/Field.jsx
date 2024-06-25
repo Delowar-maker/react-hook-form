1b86ebf5c8bf718fcaf74b2e190c0274a3e4ace5
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-25 19:20:20 +0600

    field err proble

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,3 +1,22 @@
-export default function Field() {
-  return <div></div>;
+import React from "react";
+
+export default function Field({ label, children, htmlFor, error }) {
+  const id = htmlFor || gitChildId(children);
+  return (
+    <div>
+      {label && <label htmlFor={id}>{label}</label>}
+      {children}
+      {!!error && <div>{error.message}</div>}
+    </div>
+  );
 }
+
+const gitChildId = (children) => {
+  const child = React.Children.only(children);
+  //   if ("id" in child?.props) {
+  //     return child.props.id;
+  //   }
+  if (child && "id" in child.props) {
+    return child.props.id;
+  }
+};
