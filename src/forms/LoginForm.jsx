a1de1ef8c4c96ea8693bd96f9fefea0ca766ce36
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-27 19:36:59 +0600

    loginform ui desing

--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -15,7 +15,21 @@ export default function LoginForm() {
               placeholder="Enter email address"
             />
           </Field>
+          <Field label={"Password"}>
+            <input
+              className="p-2 border box-border w-[300px] rounded-md border-gray-300"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Passwords"
+            />
+          </Field>
         </FieldSet>
+        <Field>
+          <button className="text-md text-white cursor-p p-1 border rounded-lg  bg-purple-500">
+            Login
+          </button>
+        </Field>
       </form>
     </div>
   );
